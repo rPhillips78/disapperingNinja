07e455fb04e60a72de25c6efb98f1d5aaf5ebc5e
rPhillips78 <phillipsray@ymail.com>
2019-06-30 09:12:04 -0500

    intial commit for this assignment

---
+++ js/jquery.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.ninja').click(function(){
+        $(this).hide("fast");
+    });
+
+    $('.btn').click(function(){
+        window.location.reload();
+    });
+
+
+
+
+
+});
